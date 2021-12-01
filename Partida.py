234796e53b1037329bb9546f48a06cad55a1a700
Lucas Alves <lucasalves382@hotmail.com>
2021-12-01 10:52:42 -0300

    Added a functionality to Dataclass(Partida) that take the winner of each match and print the used champions and the champions against.

--- Partida.py
+++ Partida.py
@@ -16,3 +16,13 @@ class Partida:
     def __str__(self):
         return f'{self.date},{self.patch},{self.team_blue_side},{self.team_red_side},{self.winner},{self.bans_blue_side},' \
                f'{self.bans_red_side},{self.picks_blue_side},{self.picks_red_side}'
+
+    def get_winner(self):
+        if self.winner == self.team_blue_side:
+            print(f'{self.winner} Venceu do lado azul')
+            print(f'Usando {self.picks_blue_side}')
+            print(f'Contra {self.picks_red_side}')
+        else:
+            print(f'{self.winner} Venceu do lado vermelho')
+            print(f'Usando {self.picks_red_side}')
+            print(f'Contra {self.picks_blue_side}')
